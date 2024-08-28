7cadf0fdc42e52678ef3b70485d1201a0febc279
Felipe Ryo <ryofeliperyo@gmail.com>
2024-08-28 15:00:56 -0300

    adicionando .js nos imports

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 import multer from 'multer'
-import multerConfig from './config/multer'
-import authMiddleware from './app/middlewares/auth'
+import multerConfig from './config/multer.js'
+import authMiddleware from './app/middlewares/auth.js'
 
-import UserContoller from './app/controllers/UserController'
-import SessionController from './app/controllers/SessionController'
-import ProductController from './app/controllers/ProductController'
-import CategoryController from './app/controllers/CategoryController'
-import OrderController from './app/controllers/OrderController'
+import UserContoller from './app/controllers/UserController.js'
+import SessionController from './app/controllers/SessionController.js'
+import ProductController from './app/controllers/ProductController.js'
+import CategoryController from './app/controllers/CategoryController.js'
+import OrderController from './app/controllers/OrderController.js'
 
 const routes = new Router
 
